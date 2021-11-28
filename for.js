3f46c70a3e4eee38328a887501af877d102f458b
Naillton <Nailton_junior@protonmail.com>
2021-11-28 14:47:47 -0600

    for devmedia

---
+++ for.js
@@ -0,0 +1,20 @@
+let valor_produto = 500;
+
+for (let contador = 1; contador <=5; contador++ ){
+
+    let valor_parcela = valor_produto / contador;
+    //console.log("N de parcelas "+ contador +" - valor da parcela: R$" + valor_parcela.toFixed(2));
+}
+
+
+for (let i = 0; i <= 11; i++){
+    console.log("O numero e: "+ i);
+    i++
+}
+
+let cont = 5;
+
+while(cont <= 20){
+    console.log("\nO numero e: "+cont);
+    cont++
+}
