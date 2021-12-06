56f70e686ff6d8a43e1d5a59d2fc17cddd71fe2e
Naillton <Nailton_junior@protonmail.com>
2021-12-06 09:32:45 -0600

    loop infinito while

---
+++ loopInfinite.js
@@ -0,0 +1,12 @@
+let numero = 3
+let cont = 0
+
+while(cont <= 10){
+
+    let result = numero * cont
+
+    console.log(numero+" * "+cont+" = "+result);
+
+    //retirando o cont++ teremos o loop infinito
+    //cont++
+}
