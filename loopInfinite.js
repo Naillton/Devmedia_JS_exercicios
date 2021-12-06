9839ba9d2c22a4bd99b5928c0176823ae9a0050f
Naillton <Nailton_junior@protonmail.com>
2021-12-06 10:06:02 -0600

    loop infitnito

--- loopInfinite.js
+++ loopInfinite.js
@@ -1,4 +1,4 @@
-let numero = 3
+/* let numero = 3
 let cont = 0
 
 while(cont <= 10){
@@ -9,4 +9,22 @@ while(cont <= 10){
 
     //retirando o cont++ teremos o loop infinito
     //cont++
+} */
+
+/* let contador = 0;
+
+let meses = ["Janeiro","Fevereiro","Marco","Abril"]
+
+do {
+
+    console.log(meses[contador])
+    //retirando o contador temos o loop infinito
+    //contador ++
+}while(contador < meses.length) */
+
+let doces = ["Batom", "M&Ms", "Ovo de Pascoa", "Nutella"];
+//se usarmos o i++ o contador passara contando os valores
+//se tirarmos o i++ ele fara o loop infinito
+for(let i = 0; i < doces.length; i++){
+    console.log(doces[i]);
 }
