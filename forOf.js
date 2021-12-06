033f3d60a738fc392f98d14ea38eef8a9cc50ee3
Naillton <Nailton_junior@protonmail.com>
2021-12-06 08:16:08 -0600

    exercicio de produtos com desonto em objetos com for of

--- forOf.js
+++ forOf.js
@@ -1,4 +1,4 @@
-let clientes = [
+/* let clientes = [
     {Nome: "Marcos", Telefone: "99999-0099"},
     {Nome: "Sergio", Telefone: "98989-1099"},
     {Nome: "Carlos", Telefone: "90969-2299"}
@@ -9,4 +9,24 @@ for(let client of clientes){
     let numero = client.Telefone;
 
     console.log("Nome: "+ nome +" Telefone: "+ numero);
+} */
+
+let produtos_array = [
+    { id: 1, nome: "mouse", preco: 50 },
+    { id: 2, nome: "teclado", preco: 90 },
+    { id: 3, nome: "monitor", preco: 350 },
+    { id: 4, nome: "nobreak", preco: 600 },
+];
+
+for(let prod of produtos_array){
+
+    let nome = prod.nome
+    let preco = prod.preco
+
+    let desconto = 10;
+
+    let price = (preco * desconto) / 100
+    let preco_desconto = preco - price;
+
+    console.log("Nome: "+nome+" Preco: "+preco+" com 10% de desconto: "+ preco_desconto);
 }
