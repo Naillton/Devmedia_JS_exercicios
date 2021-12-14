c31545410bb424e1433f2a054038a9c3a8770ea8
Naillton <Nailton_junior@protonmail.com>
2021-12-14 13:12:47 -0600

    nomes minusculos e maiusculos

---
+++ upperAndLower.js
@@ -0,0 +1,7 @@
+let nomeMai = "Nailton"
+let nomeMin = "nailton"
+if(nomeMai.toLowerCase() === nomeMin.toLowerCase()){
+    console.log("as palavras sao iguais")
+}else{
+    console.log("as palavras sao diferentes")
+}
