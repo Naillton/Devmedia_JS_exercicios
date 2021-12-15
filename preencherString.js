4d0063846fc56aa85862641e080890b8e8c5258d
Naillton <Nailton_junior@protonmail.com>
2021-12-15 12:56:33 -0600

    adcionando padStart e padEnd em strings

---
+++ preencherString.js
@@ -0,0 +1,14 @@
+//utilizando o padstart podemos adicionar valores no inicio de uma string
+//utilizando padEnd podemos adcionar valores ao final de cada string
+/* let label_aluno = "Aluno";
+let valor_aluno = "Edmilson";
+
+let label_email = "Email";
+let valor_email = "edmilson@gamil.com";
+
+let label_grau = "Grau de escolaridade";
+let valor_graus = "Ensino medio completo";
+
+console.log(label_aluno.padStart(20, " ")+": "+valor_aluno);
+console.log(label_aluno.padEnd(20, " ")+": "+valor_email)
+console.log(label_grau.padEnd(20, " ")+": "+valor_graus); */
