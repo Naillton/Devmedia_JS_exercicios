33d3c92264262bc8166a6692a68d1021e19c0ee9
Naillton <Nailton_junior@protonmail.com>
2021-12-15 13:09:04 -0600

    utilizando o replace em js

---
+++ replace.js
@@ -0,0 +1,4 @@
+//usando o metodo replace consigo trocar uma palavra ou uma letra pelo segundo parametro especificado no parenteses do metodo
+let frase = "Oh nailton estuda javascript e adora programar exercitando seus conhecimentos e adquirindo novos"
+let result = frase.replace("javascript", "JS")
+console.log(result)
