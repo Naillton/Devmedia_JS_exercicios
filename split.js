d33e454673dcd523013b214e31acf5f630722ed7
Naillton <Nailton_junior@protonmail.com>
2021-12-15 14:15:49 -0600

    dividindo string com split

---
+++ split.js
@@ -0,0 +1,18 @@
+//com o metodo split podemos dividir a nossa cadeia de caracteres em indices de um array
+//como vemos abaixo o split(" ") delimita a posicao dos caracteres por espaco
+
+/* let paises = "Brasil Argentina Espanha Inglaterra";
+
+console.log(paises)
+
+let resultado = paises.split(" ")
+
+console.log(resultado); */
+
+let nome = "Marilia Cysneiros";
+
+let array = nome.split(" ");
+
+let primeiroNome = array[0];
+
+console.log("Ola "+primeiroNome+" ficamos felizes por sua visita");
