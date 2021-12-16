32221726fbb12ff511f298d7384fbc2b4320a064
Naillton <Nailton_junior@protonmail.com>
2021-12-16 11:15:44 -0600

    manipulando data

---
+++ data.js
@@ -0,0 +1,7 @@
+//podemos criar datas utilizando o new Date() que nos retornara a data e hora do sistema
+const dataAtual = new Date();
+//console.log(dataAtual)
+//se quisermos passar valores de datas, podemos colocar todas nos parametros Date
+const modify = new Date( "2021/10/10" );
+console.log(modify)
+//com a opcao de selecionar valores nos parametros podemos passar data e hora para definirmos separando todas por virgula
