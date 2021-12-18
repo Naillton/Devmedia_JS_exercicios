c20916b442a91179945a61d8885adbd1c4568efb
Naillton <Nailton_junior@protonmail.com>
2021-12-18 17:04:28 -0600

    editando funcao

--- function.js
+++ function.js
@@ -7,12 +7,14 @@ console.log(soma(5, 5))
 
 function multi(number, number1){
     let media = (number + number1) / 2;
-    
+    let status = "";
     if(media >= 6){
-        return "Aprovado"
+        status = "Aprovado"
     }else if(media < 6){
-        return "Reprovado"
+        status = "Reprovado"
     }
+
+    return status
 }
 let resultado = multi(10, 1)
-console.log(resultado)
+console.log("O aluno esta "+resultado)
