ed967ad70e4ac15b018ad3db6ad34050b38a0c77
Naillton <Nailton_junior@protonmail.com>
2021-12-31 14:42:41 -0600

    Ultimo commit de 2021 projeto meteoro

---
+++ projeto_meteoro/funcoes/funcoesInterface.js
@@ -0,0 +1,13 @@
+const imprimeListaChuva = (lista) => {
+    console.log('\nNOME DO METEORO')
+
+    lista.forEach( imprimeListaChuva );
+}
+
+const imprimeChuva = (chuva) => {
+
+}
+
+(chuva)=> listarChuva(chuva, dataAtual)
+
+export default imprimeListaChuva
---
+++ projeto_meteoro/funcoes/funcoesLogicas.js
@@ -0,0 +1,10 @@
+const verificaChuvaOcorrendo = (chuva, data) => {
+
+    let data = new Date()
+    
+}
+
+const verificaProximaChuva = (chuva, data) => {
+
+    let data = new Date()
+}
